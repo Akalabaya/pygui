e40819ad3cd79d6747df6b6cb44b8d99fc94ec5f
AKALABAYA PAL <65589371+Akalabaya@users.noreply.github.com>
2022-12-03 23:25:00 +0530

    Delete createapp.bat

--- createapp.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd %1%
-echo [*] Installing reuired modules...
-"Scripts/pip" install -r requirements.txt
-echo [*] Project Created at %1%
-echo [*] Activating virtual environment...
-"Scripts/activate.bat"
-pause
